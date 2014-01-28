4bb099cdd8d1a4fdf00b758529b74f124480a75d
Robert Gerald Porter <rob@weeverapps.com>
2014-01-28 10:44:09 -0500

    Restores $_GET and cookie functions required for embed script

--- mdetect.js
+++ mdetect.js
@@ -242,6 +242,39 @@ var initCompleted = false,
 	
 	//Holds the User Agent string value.
 	uagent = '';
+	
+	function $_GET(q,s)
+	{
+	    s = s ? s : window.location.search;
+	    console.log( window.location.href );
+	    var re = new RegExp('&'+q+'(?:=([^&]*))?(?=&|$)','i');
+	    return (s=s.replace(/^\?/,'&').match(re)) ? (typeof s[1] == 'undefined' ? '' : decodeURIComponent(s[1])) : undefined;
+	}
+	
+	function createCookie(name,value,days) {
+	        if (days) {
+	                var date = new Date();
+	                date.setTime(date.getTime()+(days*24*60*60*1000));
+	                var expires = "; expires="+date.toGMTString();
+	        }
+	        else var expires = "";
+	        document.cookie = name+"="+value+expires+"; path=/";
+	}
+	
+	function readCookie(name) {
+	        var nameEQ = name + "=";
+	        var ca = document.cookie.split(';');
+	        for(var i=0;i < ca.length;i++) {
+	                var c = ca[i];
+	                while (c.charAt(0)==' ') c = c.substring(1,c.length);
+	                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	        }
+	        return null;
+	}
+	
+	function eraseCookie(name) {
+	        createCookie(name,"",-1);
+	}
    
 	//Initializes key MobileEsp variables
 	function InitDeviceScan() {
